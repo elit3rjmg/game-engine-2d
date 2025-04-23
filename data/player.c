a4d17b5eea96d0ca5aab02a23048ca8b7e9b432f
elit3rjmg <84078883+elit3rjmg@users.noreply.github.com>
2025-04-24 01:37:33 +0530

    Updated player data for basic movement

--- data/player.c
+++ data/player.c
@@ -5,22 +5,59 @@ void InitPlayer(Player *player) {
 
 	player->position.x = GetScreenWidth()/2.0f;
 	player->position.y = GetScreenHeight()/2.0f;
-	player->speed = 200.0f;
-	player->size = 40;
+    player->speed = 200;
+    player->size = 32;
+    player->textures[DIR_NORTH] = LoadTexture("assets/ant_n.png");
+    player->textures[DIR_NORTHWEST] = LoadTexture("assets/ant_wn.png");
+	player->textures[DIR_NORTHEAST] = LoadTexture("assets/ant_ne.png");
+	player->textures[DIR_EAST] = LoadTexture("assets/ant_e.png");
+	player->textures[DIR_SOUTHEAST] = LoadTexture("assets/ant_es.png");
+	player->textures[DIR_SOUTH] = LoadTexture("assets/ant_s.png");
+	player->textures[DIR_SOUTHWEST] = LoadTexture("assets/ant_sw.png");
+	player->textures[DIR_WEST] = LoadTexture("assets/ant_w.png");
 
+	player->direction = DIR_NORTH; 
 }
 
 void UpdatePlayer(Player *player) {
 
-	if (IsKeyDown(KEY_W)) player->position.y -= player->speed * GetFrameTime();
-	if (IsKeyDown(KEY_S)) player->position.y += player->speed * GetFrameTime();
-	if (IsKeyDown(KEY_A)) player->position.x -= player->speed * GetFrameTime();
-	if (IsKeyDown(KEY_D)) player->position.x += player->speed * GetFrameTime();
+	bool up = IsKeyDown(KEY_W);
+	bool down = IsKeyDown(KEY_S);
+	bool left = IsKeyDown(KEY_A);
+	bool right = IsKeyDown(KEY_D);
 
+	if (up && left) player->direction = DIR_NORTHWEST;
+	else if (up && right) player->direction = DIR_NORTHEAST;
+	else if (down && right) player->direction = DIR_SOUTHEAST;
+	else if (down && left) player->direction = DIR_SOUTHWEST;
+	else if (up) player->direction = DIR_NORTH;
+	else if (down) player->direction = DIR_SOUTH;
+	else if (left) player->direction = DIR_WEST;
+	else if (right) player->direction = DIR_EAST;
+
+	float dt = GetFrameTime();
+	if (up) player->position.y -= player->speed * dt;
+	if (down) player->position.y += player->speed * dt;
+	if (left) player->position.x -= player->speed * dt;
+	if (right) player->position.x += player->speed * dt;
 }
 
 void DrawPlayer(Player *player) {
-	DrawRectangle(GetScreenWidth() / 2 - player->size / 2,
-                  GetScreenHeight() / 2 - player->size / 2,
-                  player->size, player->size, BLUE);
+    float scale = 0.25f;
+    Texture2D tex = player->textures[player->direction];
+
+    Vector2 position = {
+        GetScreenWidth() / 2.0f - (tex.width * scale) / 2.0f,
+        GetScreenHeight() / 2.0f - (tex.height * scale) / 2.0f
+    };
+
+    DrawTextureEx(tex, position, 0.0f, scale, WHITE);
+}
+
+void UnloadPlayer(Player *player) {
+
+    for (int i = 0; i < 7; i++) {
+    	UnloadTexture(player->textures[i]);
+	}
+
 }
--- data/player.h
+++ data/player.h
@@ -1,16 +1,24 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
-typedef struct Player {
-	Vector2 position;
-	Vector2 cameraOffset;
-	float speed;
-	int size;
+typedef enum {
+    DIR_NORTH, DIR_NORTHWEST, DIR_NORTHEAST, DIR_EAST,
+    DIR_SOUTHEAST, DIR_SOUTH, DIR_SOUTHWEST,
+    DIR_WEST
+} Direction;
 
+typedef struct Player {
+    Vector2 position;
+    Vector2 cameraOffset;
+    float speed;
+    int size;
+    Texture2D textures[8]; // One for each direction
+    Direction direction;
 } Player;
 
 void InitPlayer(Player *player);
 void UpdatePlayer(Player *player);
 void DrawPlayer(Player *player);
+void UnloadPlayer(Player *player);
 
 #endif
